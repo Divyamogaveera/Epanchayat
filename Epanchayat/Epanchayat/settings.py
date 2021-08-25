dfa191faf3541889245fb587d62f47201b9e5caa
Divyamogaveera <divyashekhar4444@gmail.com>
2021-08-25 02:04:39 -0500

    email

--- Epanchayat/Epanchayat/settings.py
+++ Epanchayat/Epanchayat/settings.py
@@ -143,8 +143,7 @@ AUTH_USER_MODEL='myapp.User'
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587 #outgoing email
-EMAIL_HOST_USER='epanchayatservice@gmail.com'
-EMAIL_HOST_PASSWORD='nkybkxmcwpynzfnm'
+
 EMAIL_USE_TLS=True
 
 
