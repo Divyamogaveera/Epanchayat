0384bc08ade5b9fea11a91dd2b0b1d7d6f8c18da
Divyamogaveera <divyashekhar4444@gmail.com>
2021-11-09 19:25:15 +0700

    ending

--- ppp/diya/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-   gender = models.BooleanField(default=True)
-   grampanchayat=models.CharField(default="gram",max_length=122) # True for male and False for female
-   # you can add more fields here.
