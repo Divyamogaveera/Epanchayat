161f3e2df9b6b58132066a2cb6938a4a8687ebdd
Divyamogaveera <divyashekhar4444@gmail.com>
2021-11-10 00:42:21 +0700

    safety

--- Epanchayat/Epanchayat/settings.py
+++ Epanchayat/Epanchayat/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+n_-4*tr)t))cin=vrz$4#dil3%l(!h0__wd$75d-#mis=9qf9'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
